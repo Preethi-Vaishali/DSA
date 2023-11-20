de6ffb2114a072246e92ff4cc63192fd8ab00748
Preethi_Vaishali <preethi.vaishali.806pcs@gmail.com>
2023-11-20 10:59:09 +0530

    Update Commit

---
+++ src/Linked_List/MyLinkedList.java
@@ -0,0 +1,15 @@
+package Linked_List;
+
+import java.util.LinkedList;
+
+public class MyLinkedList<E> extends LinkedList<E>{
+public MyLinkedList() {
+	super();
+}
+@Override
+public int size() {
+	super.size();
+	return 0;
+}
+
+}
---
+++ src/Linked_List/MyLinkedListMain.java
@@ -0,0 +1,13 @@
+package Linked_List;
+
+public class MyLinkedListMain {
+
+	public static void main(String[] args) {
+		MyLinkedList<String> mll = new MyLinkedList<String>();
+		mll.add("Ranjith");
+		mll.add("Karthi");
+		mll.add("Vaishu");
+		System.out.println(mll.size());;
+	}
+
+}
